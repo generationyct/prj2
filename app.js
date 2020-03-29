6d4812c180d6f09e66402da2b66f0c8083524472
Devgen <dev@generationyct.nl>
2020-03-29 17:34:18 +0200

    Added styling and partials from Jonas

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError     = require('http-errors');
+const express         = require('express');
+const path            = require('path');
+const hbs             = require('hbs');
+const mongoose        = require('mongoose');
+const bodyParser      = require('body-parser');
+const favicon         = require('serve-favicon');
+const passport        = require('passport');
+const cookieParser    = require('cookie-parser');
+const logger          = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
+
+// use mongoose
+
+mongoose
+  .connect('mongodb://localhost/ironfood', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +34,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// register partials
+hbs.registerPartials(__dirname + "/views/partials")
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
