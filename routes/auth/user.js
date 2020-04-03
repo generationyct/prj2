5eef4fe932bb01632669b837ce695ce76bbdefea
Devgen <dev@generationyct.nl>
2020-04-03 14:49:38 +0200

    Added a jwt token function to the signup route so that users have direct access after signin up

--- routes/auth/user.js
+++ routes/auth/user.js
@@ -20,7 +20,8 @@ userRouter.post('/signup', async (req, res) => {
 
   try {
     await user.save()
-    res.status(201).send({ user })
+    const token = await user.generateAuthToken()
+    res.status(201).send({ user, token })
 
   } catch (err) {
     res.status(400).send({ err })
