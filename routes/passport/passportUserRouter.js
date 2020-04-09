2bc1f8f5f7b8140e92539e0191acb6777d6f3375
Devgen <dev@generationyct.nl>
2020-04-09 10:23:18 +0200

    Added checks on registration

--- routes/passport/passportUserRouter.js
+++ routes/passport/passportUserRouter.js
@@ -12,4 +12,39 @@ passportUserRouter.get('/register', (req, res, next) => {
   res.render('passport/register', { title: 'Iron Food Passport' });
 });
 
+// register Post handler
+passportUserRouter.post('/register', (req, res) => {
+  const { name, email, password, confirmpassword } = req.body
+  let errors = []
+
+  // check required fields
+  if(!email || !password || confirmpassword) {
+    errors.push({ msg: 'Please fill in all required fields'})
+  }
+
+  // check passwords match
+  if(password !== confirmpassword) {
+    errors.push({ msg: 'Passwords do not match'})
+  }
+
+  // check password length
+  if(password.length <6 ) {
+    errors.push({ msg: 'Password should be at least 6 characters'})
+  }
+
+  if(errors.length > 0) {
+    res.send( {
+      errors,
+      name,
+      email,
+      password,
+      confirmpassword
+  })
+
+  }else {
+    res.send('pass registration')
+  }
+
+})
+
 module.exports = passportUserRouter
