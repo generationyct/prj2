925e0513839d3e92621baed30dfdbdfb0816676f
Jonas <jonas@smartvizual.com>
2020-04-10 17:26:53 +0200

    tips-add form works now

--- routes/tips.js
+++ routes/tips.js
@@ -13,29 +13,47 @@ tipRouter.get('/tips-detail', (req, res) => {
     res.render('tips-detail')
 })
 
-tipRouter.get('/tips-add', ensureAuthenticated, (req, res) => {
+// tipRouter.get('/tips-add', ensureAuthenticated, (req, res) => {
+//     res.render('tips-add')
+// })
+
+tipRouter.get('/tips-add', (req, res) => {
     res.render('tips-add')
 })
 
-tipRouter.post('/tips', async (req, res) => {
-    // const tip = new Tip(req.body)
-    // await req.user.populate('tip').execPopulate()
-    // console.log(req.user._id);
-    const tip = new Tip({
-        ...req.body,
-        // author: req.user._id
+tipRouter.post('/tips', (req, res, next) => {
+    console.log('Post route triggered');
+    const { name, category, description, website, address, imageUrl, date, author } = req.body;
+    const newTip = new Tip({ name, category, description, website, address, imageUrl, date, author })
+    console.log(Tip);
+    newTip.save()
+    .then((tip) => {
+      res.redirect('/tips');
     })
+    .catch((error) => {
+      console.log(error);
+    })
+});
 
-    try {
-        await tip.save()
-        // const token = await user.generateAuthToken()
-        // res.cookie('auth_token', token)
-        res.render('tips')
+// tipRouter.post('/tips', async (req, res) => {
+//     // const tip = new Tip(req.body)
+//     // await req.user.populate('tip').execPopulate()
+//     // console.log(req.user._id);
+//     const tip = new Tip({
+//         ...req.body,
+//         // author: req.user._id
+//     })
 
-    } catch (err) {
-        res.status(400).send({ err })
-    }
-})
+//     try {
+//         await tip.save()
+//         // const token = await user.generateAuthToken()
+//         // res.cookie('auth_token', token)
+//         res.render('tips')
+
+//     } catch (err) {
+//         res.status(400).send({ err })
+//     }
+// })
 
 
 
