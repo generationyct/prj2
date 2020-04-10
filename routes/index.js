af7aff48f7320b2d6251db276fd08c584e4dcc2b
Devgen <dev@generationyct.nl>
2020-04-10 12:38:50 +0200

    Refactored the tips routes

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Iron Food' });
-});
-
-
-router.get('/tips-detail', function(req, res, next) {
-  res.render('tips-detail', { title: 'Tips detail' });
-});
-
-router.get('/tips-add', function(req, res, next) {
-  res.render('tips-add', { title: 'Add your food tip to Iron Food' });
 });
 
 module.exports = router;
