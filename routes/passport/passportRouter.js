2e4c564bfad8d933870f28bee31dca56d71ad0f3
Devgen <dev@generationyct.nl>
2020-04-11 13:59:47 +0200

    Removed dubble index.hbs not needed and changed index route to use /views/index.hbs

--- routes/passport/passportRouter.js
+++ routes/passport/passportRouter.js
@@ -4,7 +4,7 @@ const passportRouter = express.Router()
 
 passportRouter.get('/', (req, res, next) => {
   // res.send('Hi from the passport route');
-  res.render('passport/index', { title: 'Iron Food Passport' });
+  res.render('index', { title: 'Iron Food Passport' });
 });
 
 module.exports = passportRouter
